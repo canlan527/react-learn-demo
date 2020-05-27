e4dc80d4a6a83712a54114c23034f38f64cb7537
canlandazzle <1481086138@qq.com>
2020-05-27 19:02:35 +0800

    图片奇幻

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import imgUrl1 from './assets/1.jpg'
+import imgUrl2 from './assets/2.jpg'
+import imgUrl3 from './assets/3.jpg'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById('root')
+const imgArr = [imgUrl1, imgUrl2, imgUrl3]
+let index = 0
+let timer = null
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function start() {
+  clearInterval(timer)
+  timer = setInterval(() => {
+    index = (index + 1)% 3
+    console.log(index)
+    init()
+  }, 2000)
+}
+
+function stop() {
+  clearInterval(timer)
+}
+
+function init() {
+  ReactDOM.render(
+    <img src={imgArr[index]} alt={''} />,
+    root
+  );
+}
+init()
+start()
+root.onmouseenter = function () {
+  stop()
+}
+root.onmouseleave = function () {
+  start()
+}
