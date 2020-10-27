94071dfdaf2a6a50a7686f7338e647b72e95d6f4
canlan0711 <1401086138@qq.com>
2020-10-27 12:10:51 +0800

    jsx

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,41 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import imgUrl1 from './assets/1.jpg'
-import imgUrl2 from './assets/2.jpg'
-import imgUrl3 from './assets/3.jpg'
 
-const root = document.getElementById('root')
-const imgArr = [imgUrl1, imgUrl2, imgUrl3]
-let index = 0
-let timer = null
-
-function start() {
-    stop()
-    timer = setInterval(() => {
-        index = (index + 1) % 3
-        console.log(index)
-        init()
-    }, 2000)
-}
-
-function stop() {
-    clearInterval(timer)
-}
-
-function init() {
-    ReactDOM.render( <
-        img src = { imgArr[index] }
-        alt = { '' }
-        />,
-        root
-    );
-}
-init()
-start()
-root.onmouseenter = function() {
-    stop()
-}
-root.onmouseleave = function() {
-    start()
-}
+let num = 0;
+setInterval(() => {
+    num ++;
+    const div = (<>
+        {num}
+    </>)
+    ReactDOM.render(div, document.getElementById('root'))
+},1000);
