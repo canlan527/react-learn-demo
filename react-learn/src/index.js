15561576e20e25e604156b42297b54ffedadd65c
canlandazzle <1481086138@qq.com>
2020-12-04 22:51:42 +0800

    react-event

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NewTickControl from './components/NewTickControl'
 const root = document.getElementById('root');
+// function handleClick() {
+//   console.log('点击了')
+// }
+// const btn = <button onClick={handleClick}>点击我</button>
+const btn = <button onClick={() => {
+  console.log('点击了')
+}}>点击我</button>
 
-
-
-ReactDOM.render(<NewTickControl />,root)
+ReactDOM.render(btn,root)
