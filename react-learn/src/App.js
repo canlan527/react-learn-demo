29baf154c03d188dc83055beb195ecf635c07bf3
canlandazzle <1481086138@qq.com>
2021-01-03 17:40:07 +0800

    ref

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,29 +1,11 @@
-/*
- * @Author: your name
- * @Date: 2020-12-28 15:48:55
- * @LastEditTime: 2020-12-28 17:33:18
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \react-learn\src\App.js
- */
-import React from 'react'
-// import WithTest from './HOC/WithTest'
-import { A, B } from './components/Comps'
-import withLog from './HOC/withLog'
-import withLogin from './HOC/withLogin'
+import React, { Component } from 'react'
 
-// let AComp = withLogin(A)
-// AComp = withLog(AComp)
-// let BComp = withLogin(B)
-// BComp = withLog(BComp)
-
-let AComp = withLog(withLogin(A))
-let BComp = withLog(withLogin(B))
-export default function App() {
-  return (
-    <div>
-      <AComp isLogin a={1} />
-      <BComp isLogin b={2} />
-    </div>
-  )
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ref</h1>
+      </div>
+    )
+  }
 }
