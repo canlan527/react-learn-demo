1ef3192af0ecc80b88458aa41bf2032686d71aca
canlandazzle <1481086138@qq.com>
2021-01-23 23:08:25 +0800

    ref转发

---
+++ react-learn/src/App copy.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+// import Comp from './components/Comp'
+import Test from './components/common/Banner/Test'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <Comp></Comp> */}
+        <Test></Test>
+      </div>
+    )
+  }
+}
--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
-// import Comp from './components/Comp'
-import Test from './components/common/Banner/Test'
+
+class A extends Component {
+  render() {
+    return <h1 ref={this.props.abc}>
+              组件A
+        <span>{this.props.word}</span>
+    </h1>
+  }
+}
+
+const NewA = React.forwardRef((props, ref) => {
+  return <A {...props} abc={ref} />
+})
+// function A(props,ref) {
+//   console.log(props, ref)
+//   return <h1 ref={ref}>
+//       组件A
+
+//     <span>{props.word}</span>
+//   </h1>
+// }
+
+// 传递函数组件A, 得到一个新组件NewA
+// const NewA = React.forwardRef(A)
 export default class App extends Component {
+
+  componentDidMount() {
+    console.log(this.ARef)
+  }
+  
+  ARef = React.createRef()
   render() {
     return (
       <div>
-        {/* <Comp></Comp> */}
-        <Test></Test>
+       <NewA ref={this.ARef} word={'12312312'} />
+       {/* 想得到A组件内部的h1元素 */}
       </div>
     )
   }
